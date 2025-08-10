7380e8524355fd1eb9fbe8463e314e8e327daf64
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2025-08-10 11:19:04 +0000

    refactor: Refactor Downloader class for efficiency and typing
    - Instantiates yt_dlp.YoutubeDL only once to improve performance.
    - Adds comprehensive type hints to the class and its methods.
    - Consolidates download logic into a private helper method to reduce code duplication.
    - Fixes a bug where downloading would fail if no video was selected.

--- utils/Downloader.py
+++ utils/Downloader.py
@@ -1,44 +1,50 @@
+from typing import List
+
 import yt_dlp
 
 from utils.Config import Config
+from utils.Playlist import AudioTrack, SubTrack, Video
 from utils.retry import retry
 
 
 class Downloader:
     def __init__(self, config: Config, yt_dlp_config: dict):
-        self.name = config.name
-        self.selected_video = config.selected_video
-        self.selected_audio = config.selected_audio
-        self.selected_subs = config.selected_subs
+        self.name: str = config.name
+        self.selected_video: Video | None = config.selected_video
+        self.selected_audio: List[AudioTrack] | None = config.selected_audio
+        self.selected_subs: List[SubTrack] | None = config.selected_subs
 
-        self.options = yt_dlp_config
+        self.options: dict = yt_dlp_config
+        self.downloader = yt_dlp.YoutubeDL(self.options)
 
-    def download_video(self, video):
-        options = self.options
+    def _download_item(self, url: str, output_template: str) -> None:
+        original_outtmpl = self.downloader.params.get("outtmpl")
+        try:
+            self.downloader.params["outtmpl"] = output_template
+            retry(max_attempts=10, func=self.downloader.download, url_list=[url])
+        finally:
+            if original_outtmpl is not None:
+                self.downloader.params["outtmpl"] = original_outtmpl
+            elif "outtmpl" in self.downloader.params:
+                del self.downloader.params["outtmpl"]
+
+    def download_video(self, video: Video) -> None:
         output_template = f"{self.name}/{self.name}.mp4"
-        ydl_opts = {**options, "outtmpl": output_template}
-        downloader = yt_dlp.YoutubeDL(ydl_opts)
-        retry(max_attempts=10, func=downloader.download, url_list=[video.url])
+        self._download_item(video.url, output_template)
 
-    def download_audio(self, audio):
-        for index, track in enumerate(audio):
+    def download_audio(self, audio_tracks: List[AudioTrack]) -> None:
+        for index, track in enumerate(audio_tracks):
             output_template = f"{self.name}/{self.name}-audio-{index}.mp4"
-            ydl_opts = {**self.options, "outtmpl": output_template}
-            downloader = yt_dlp.YoutubeDL(ydl_opts)
-            retry(max_attempts=10, func=downloader.download, url_list=[track.url])
+            self._download_item(track.url, output_template)
 
-    def download_subs(self, subs):
-        for index, track in enumerate(subs):
+    def download_subs(self, subs_tracks: List[SubTrack]) -> None:
+        for index, track in enumerate(subs_tracks):
             output_template = f"{self.name}/{self.name}-subs-{index}.mp4"
-            ydl_opts = {
-                **self.options,
-                "outtmpl": output_template,
-            }
-            downloader = yt_dlp.YoutubeDL(ydl_opts)
-            retry(max_attempts=10, func=downloader.download, url_list=[track.url])
+            self._download_item(track.url, output_template)
 
-    def download(self):
-        self.download_video(self.selected_video)
+    def download(self) -> None:
+        if self.selected_video:
+            self.download_video(self.selected_video)
         if self.selected_audio:
             self.download_audio(self.selected_audio)
         if self.selected_subs:
